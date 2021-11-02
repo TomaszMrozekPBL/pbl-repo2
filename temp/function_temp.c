83023ddc9c14246b8a556952aec7ea2fe6ee61b3
TomaszMrozekPBL <t.mrozek@tele.pw.edu.pl>
2021-11-02 19:44:51 +0100

    Added new folder with files

---
+++ temp/function_temp.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+void temp_fnc() {
+	puts("Empty function");
+	return 0;
+}
---
+++ temp/main_temp.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main() {
+	puts("Hello world");
+	puts("Second commit, example");
+	return 0;
+}
